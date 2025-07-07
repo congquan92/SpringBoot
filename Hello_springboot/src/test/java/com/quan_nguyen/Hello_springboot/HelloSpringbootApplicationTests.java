ac88c177d69e80311bfc3a6f6a404a5f3d7e7ee2
congquan92 <145172432+congquan92@users.noreply.github.com>
2025-07-07 14:20:10 +0700

    Bai 1 ( Hello World Spring Boot )

---
+++ Hello_springboot/src/test/java/com/quan_nguyen/Hello_springboot/HelloSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quan_nguyen.Hello_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
